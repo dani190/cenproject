b323bdfb1432481483e848d6f1bb7afa42404da9
Adam Chow <adamdy25@gmail.com>
2012-12-06 15:56:57 -0500

    remove old files

--- app/assets/javascripts/reloader.js
+++
@@ -1,1 +0,0 @@
-//setTimeout("location.reload(true);", 10000);
--- app/controllers/admin/main_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::MainController < ApplicationController
-  before_filter :authenticate_admin!
-  respond_to :html
-  
-  def index
-    
-  end
-  
-end
